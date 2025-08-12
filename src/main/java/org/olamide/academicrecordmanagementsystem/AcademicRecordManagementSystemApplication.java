ce434209cc5749dde61e22752cf3ab155b765552
Olamide <a.olamide@wallzandqueenltd.com>
2025-08-11 19:07:59 -0500

    First push - Program Sructure Completed

---
+++ src/main/java/org/olamide/academicrecordmanagementsystem/AcademicRecordManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.olamide.academicrecordmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicRecordManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicRecordManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/olamide/academicrecordmanagementsystem/AcademicRecordManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.olamide.academicrecordmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicRecordManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
